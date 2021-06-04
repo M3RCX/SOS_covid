852f09d7056316c5b62214df3f5ea44d6d48fba4
Thiago820131468 <bonette_36@hotmail.com>
2021-06-04 00:00:44 -0300

    serviço de login e cadastramento funcionando

--- backend/microservicos/login/src/index.js
+++ backend/microservicos/login/src/index.js
@@ -3,7 +3,7 @@ const cliente = require('../../cadastro/src/cliente');
 const app = express();
 app.use(express.json());
 
-app.use( 
+app.use(
     (req, res, next) => {
 
         res.setHeader('Access-Control-Allow-Origin', "*");
@@ -17,17 +17,20 @@ app.use(
 app.post('/login', function (req, res) {
     let cpf = req.body.cpf; //CPF vindo do formulário de login
     let senha = req.body.senha; //Senha vinda do formulário de login
-
+//{attributes:['cpf, senha']}
+//cpf == clientes[i].cpf && senha == clientes[i].senha
     
-        cliente.findAll(cpf, senha).then((clientes) => {
-            for (int = 0; i < clientes.lenght; i++) {
+        cliente.findAll({attributes:['cpf', 'senha' ]}).then((clientes) => {
+            for (i= 0; i < clientes.length; i++) {
                 if (cpf == clientes[i].cpf && senha == clientes[i].senha) {
                     console.log('Bem-Vindo');
                 } else {
-                    res.end("Usuário ou senha não existe")
+                    console.log("Usuário ou senha não existe")
                 }
             }
-        })
+        }).catch(function (erro) {
+            console.log("Não foi possível consultar o banco" + erro);
+        });
 });
 
 app.listen(5000);
